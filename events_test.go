3544ed4a3bf1ef3557deaef511488b456c2d8238
Daniel J Blueman <daniel@numascale.com>
2019-05-01 13:07:00 +0800

    Testing fixes

--- events_test.go
+++ events_test.go
@@ -9,8 +9,8 @@ func TestMain(m *testing.M) {
    fmt.Println("TestMain")
    dev := &Numaconnect2{}
 
-   if dev.probe() {
-      dev.enable([]uint16{0x68, 0x80})
+   if dev.probe() > 0 {
+      dev.enable([]uint16{0x68, 0x80}, false)
       for i := 0; i < 3; i++ {
          _ = dev.sample()
       }
