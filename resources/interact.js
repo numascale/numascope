21aa806f9cbdd51342bbe937f1a0af59351af52f
Daniel J Blueman <daniel@numascale.com>
2019-07-24 15:54:31 +0800

    Add missing boilterplates

--- resources/interact.js
+++ resources/interact.js
@@ -1,3 +1,20 @@
+/*  Copyright (C) 2019 Daniel J Blueman
+    This file is part of Numascope.
+
+    Numascope is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Numascope is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Numascope.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const graph = document.getElementById('graph')
 const btnPlay = document.getElementById('btn-play')
 const btnPause = document.getElementById('btn-pause')
